f54f4b2758176dd3fd6bf9f10e8500e95fb274d5
yudai.ito <yudai.i51698@gmail.com>
2019-01-21 04:07:32 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,23 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  def index
-    @tasks = Task.all.page(params[:page])
-  end
+  before_action :correct_user, only: [:destroy]
+  before_action :require_user_logged_in
   
   def show
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が作成されませんでした'
       render :new
     end
@@ -52,5 +52,12 @@ class TasksController < ApplicationController
   #Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def currect_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @tasklist
+      redirect_to root_url
+    end
   end
 end
